6bcf512f3ba7f5adf97b8738ce2f20044a7aebdd
Michael Bastos <bastosmichael@gmail.com>
2026-02-06 08:25:38 -0600

    Create variables.tf to define Terraform variables

--- variables.tf
+++ variables.tf
@@ -1,38 +1,64 @@
-# Terraform Variables for Kong Deployment
+variable "docker_host" {
+  description = "Docker host socket or URL"
+  type        = string
+  default     = "unix:///var/run/docker.sock"
+}
 
-variable "kong_admin_url" {
-  description = "The URL for the Kong Admin API"
+variable "local_server_ip" {
+  description = "Local server IP address for accessing Kong services"
   type        = string
 }
 
-variable "database_url" {
-  description = "The database connection string"
+variable "kong_db_user" {
+  description = "Kong database username"
   type        = string
+  sensitive   = true
 }
 
-variable "kong_consumer" {
-  description = "The Kong consumer name"
+variable "kong_db_password" {
+  description = "Kong database password"
   type        = string
+  sensitive   = true
 }
 
-variable "service_name" {
-  description = "The name of the service to be deployed"
+variable "postgres_port" {
+  description = "PostgreSQL port on the host machine"
+  type        = number
+  default     = 5432
+}
+
+variable "kong_proxy_port" {
+  description = "Kong proxy HTTP port on the host machine"
+  type        = number
+  default     = 8000
+}
+
+variable "kong_proxy_ssl_port" {
+  description = "Kong proxy HTTPS port on the host machine"
+  type        = number
+  default     = 8443
+}
+
+variable "kong_admin_port" {
+  description = "Kong admin API HTTP port on the host machine"
+  type        = number
+  default     = 8001
+}
+
+variable "kong_admin_ssl_port" {
+  description = "Kong admin API HTTPS port on the host machine"
+  type        = number
+  default     = 8444
+}
+
+variable "kong_manager_port" {
+  description = "Kong Manager GUI port on the host machine"
+  type        = number
+  default     = 8080
+}
+
+variable "volume_mount_path" {
+  description = "Host path for volume mounts"
   type        = string
+  default     = "/tmp/kong-volumes"
 }
-
-variable "service_url" {
-  description = "The URL of the service"
-  type        = string
-}
-
-variable "route_paths" {
-  description = "The paths that route to the service"
-  type        = list(string)
-}
-
-variable "enable_tls" {
-  description = "Enable TLS for the service"
-  type        = bool
-}
-
-# Additional variables can be defined based on your requirements
