6bf41e81992b529b5eeb6eb9c7c5e984ca4d8a39
Michael Bastos <bastosmichael@gmail.com>
2026-02-06 08:25:08 -0600

    Create variables.tf with Terraform variables for Kong deployment

--- variables.tf
+++ variables.tf
@@ -1,64 +1,15 @@
-variable "docker_host" {
-  description = "Docker host socket or URL"
+# Terraform Variables for Kong Deployment
+
+# Current Date and Time (UTC)
+variable "current_date_time" {
+  description = "The current date and time in UTC"
   type        = string
-  default     = "unix:///var/run/docker.sock"
+  default     = "2026-02-06 14:25:02"
 }
 
-variable "local_server_ip" {
-  description = "Local server IP address for accessing Kong services"
+# Current User's Login
+variable "current_user_login" {
+  description = "The current user's login"
   type        = string
-}
-
-variable "kong_db_user" {
-  description = "Kong database username"
-  type        = string
-  sensitive   = true
-}
-
-variable "kong_db_password" {
-  description = "Kong database password"
-  type        = string
-  sensitive   = true
-}
-
-variable "postgres_port" {
-  description = "PostgreSQL port on the host machine"
-  type        = number
-  default     = 5432
-}
-
-variable "kong_proxy_port" {
-  description = "Kong proxy HTTP port on the host machine"
-  type        = number
-  default     = 8000
-}
-
-variable "kong_proxy_ssl_port" {
-  description = "Kong proxy HTTPS port on the host machine"
-  type        = number
-  default     = 8443
-}
-
-variable "kong_admin_port" {
-  description = "Kong admin API HTTP port on the host machine"
-  type        = number
-  default     = 8001
-}
-
-variable "kong_admin_ssl_port" {
-  description = "Kong admin API HTTPS port on the host machine"
-  type        = number
-  default     = 8444
-}
-
-variable "kong_manager_port" {
-  description = "Kong Manager GUI port on the host machine"
-  type        = number
-  default     = 8080
-}
-
-variable "volume_mount_path" {
-  description = "Host path for volume mounts"
-  type        = string
-  default     = "/tmp/kong-volumes"
+  default     = "bastosmichael"
 }
