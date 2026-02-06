406a8d7669b56b95abe3d46a2ba984ca19be164d
Michael Bastos <bastosmichael@gmail.com>
2026-02-06 08:25:20 -0600

    Add variables.tf for Terraform variables related to Kong deployment

--- variables.tf
+++ variables.tf
@@ -1,15 +1,38 @@
 # Terraform Variables for Kong Deployment
 
-# Current Date and Time (UTC)
-variable "current_date_time" {
-  description = "The current date and time in UTC"
+variable "kong_admin_url" {
+  description = "The URL for the Kong Admin API"
   type        = string
-  default     = "2026-02-06 14:25:02"
 }
 
-# Current User's Login
-variable "current_user_login" {
-  description = "The current user's login"
+variable "database_url" {
+  description = "The database connection string"
   type        = string
-  default     = "bastosmichael"
 }
+
+variable "kong_consumer" {
+  description = "The Kong consumer name"
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of the service to be deployed"
+  type        = string
+}
+
+variable "service_url" {
+  description = "The URL of the service"
+  type        = string
+}
+
+variable "route_paths" {
+  description = "The paths that route to the service"
+  type        = list(string)
+}
+
+variable "enable_tls" {
+  description = "Enable TLS for the service"
+  type        = bool
+}
+
+# Additional variables can be defined based on your requirements
